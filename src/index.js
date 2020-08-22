a20a52d3eaa23ff4db0a5af44b7a4a07eec24d1f
willianmatheus98 <williantecnico1998@gmail.com>
2020-08-21 23:22:38 -0300

    primeiro bundle pelo webpack

---
+++ src/index.js
@@ -0,0 +1,9 @@
+'use strict'
+var React = require('react')
+var ReactDOM = require('react-dom')
+var Title = require('./app')
+
+ReactDOM.render(
+  React.createElement(Title),
+  document.querySelector('[data-js="app"]')
+)
